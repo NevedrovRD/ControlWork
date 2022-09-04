e61393dc701a58c695e8cc0d603326380cdfa65c
Roman <ramec154@mail.ru>
2022-09-04 16:40:23 +0300

    Добавлена программа

---
+++ Programm/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] array = { "Саша", "Маша", "12", ":-D", "Ян" };
+string[] array1 = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        array1[i] = array[i];
+}
+Console.WriteLine("[" + string.Join(" ", array1) + "]");
